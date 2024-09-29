aa7fac11a85a26aaca522855cdf9f94d534942fe
Saadat Baig <me@saadat.dev>
2024-09-29 18:44:17 +0200

    add "hello world" materials

---
+++ 01_helloworld/main.cbl
@@ -0,0 +1,14 @@
+      *> ***************************************************************
+      *> (C) COPYRIGHT Baig Software 2024. ALL RIGHTS RESERVED
+      *> ***************************************************************
+      *> PROGRAM:  HelloWorld
+      *>
+      *> AUTHOR :  Saadat Baig
+      *>
+      *> THE VERY FIRST STEPS INTO COBOL
+      *> ****************************************************************
+        IDENTIFICATION DIVISION.
+        PROGRAM-ID. HelloWorld.
+        PROCEDURE DIVISION.
+            DISPLAY 'Hello, World!'.
+            STOP RUN.
